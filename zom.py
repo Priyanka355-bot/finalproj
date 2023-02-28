3a71b5a9bd946391802494b6591b3cf1d96c7170
Tanneeru priyanka <80468447+Priyanka355-bot@users.noreply.github.com>
2023-02-28 15:18:44 +0530

    Add files via upload

---
+++ zom.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import os
+
+st.title(":orange[INNOMATICS RESEARCH LABS]")
+
+st.header("DATA SCIENCE INTERNSHIP 2023")
+st.snow()
+
+btn_click = st.button("Click Me!")
+
+if btn_click == True:
+    st.subheader("I am Priyanka Tanneeru, a working professional in the field of Edtech and also a data science enthusiast")
+    st.balloons()
